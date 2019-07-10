63a45f011dc4ab435c07c9da19b5d59b3aebfae6
pengdong.guo <pengdong.guo@ly.com>
2019-07-10 16:46:25 +0800

    图片压缩

--- src/test/java/com/example/demo/test/CompressionTest.java
+++ src/test/java/com/example/demo/test/CompressionTest.java
@@ -38,8 +38,5 @@ public class CompressionTest {
             String s = "{% fi https://cloud.gpdstudy.club/static/images/" + i + ".jpg@lazy%}\n";
             os.write(s.getBytes());
         }
-
-        //压缩图片 第一个参数是原图路径 后面那个路径是压缩以后的输出路径
-        Thumbnails.of(outFile).outputQuality(0.9f).scale(1f).toFile(outputPath);
     }
 }
