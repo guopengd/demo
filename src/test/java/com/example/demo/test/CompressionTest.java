9a4e1e52e860c9645e3e97a91bead07da21b94d0
pengdong.guo <pengdong.guo@ly.com>
2019-07-10 16:50:52 +0800

    图片压缩

--- src/test/java/com/example/demo/test/CompressionTest.java
+++ src/test/java/com/example/demo/test/CompressionTest.java
@@ -18,6 +18,7 @@ public class CompressionTest {
     public static void main(String[] args) throws IOException {
         String path = "D:\\potos";
         String outputPath = "D:\\MyConfiguration\\pengdong.guo\\Desktop\\";
+        String textPath = "D:\\MyConfiguration\\pengdong.guo\\Desktop\\a.txt";
 
         File file = new File(path);
         if (file.isFile()) {
@@ -29,14 +30,17 @@ public class CompressionTest {
             for (int i = 0; i < tempList.length; i++) {
                 outputImage(tempList[i], outputPath + i + ".jpg");
             }
+
+            OutputStream os = new FileOutputStream(new File(textPath));
+            for (int i = 0; i < tempList.length; i++) {
+                String s = "{% fi https://cloud.ovvow.com/static/images/" + i + ".jpg@lazy%}\n";
+                os.write(s.getBytes());
+            }
         }
     }
 
     private static void outputImage(File outFile, String outputPath) throws IOException {
-        OutputStream os = new FileOutputStream(new File("/home/guopengdong/文档/a.txt"));
-        for (int i = 0; i < 73; i++) {
-            String s = "{% fi https://cloud.gpdstudy.club/static/images/" + i + ".jpg@lazy%}\n";
-            os.write(s.getBytes());
-        }
+        //压缩图片 第一个参数是原图路径 后面那个路径是压缩以后的输出路径
+        Thumbnails.of(outFile).outputQuality(0.9f).scale(1f).toFile(outputPath);
     }
 }
