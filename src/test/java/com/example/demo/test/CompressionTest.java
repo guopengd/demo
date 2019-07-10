c5dadd4ca35671ab6a6f71b7cd268b4fe185ab23
pengdong.guo <pengdong.guo@ly.com>
2019-07-10 16:42:45 +0800

    图片压缩

--- src/test/java/com/example/demo/test/CompressionTest.java
+++ src/test/java/com/example/demo/test/CompressionTest.java
@@ -5,8 +5,9 @@ import net.coobird.thumbnailator.geometry.Positions;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author pengdong.guo
@@ -15,10 +16,24 @@ import java.io.IOException;
 public class CompressionTest {
 
     public static void main(String[] args) throws IOException {
+        String path = "D:\\potos";
+        String outputPath = "D:\\MyConfiguration\\pengdong.guo\\Desktop\\";
+
+        File file = new File(path);
+        if (file.isFile()) {
+            outputImage(file, outputPath);
+        }
+        if (file.isDirectory()) {
+            File[] tempList = file.listFiles();
+            assert tempList != null; // 目录存在则文件永远不会为null
+            for (int i = 0; i < tempList.length; i++) {
+                outputImage(tempList[i], outputPath + i + ".jpg");
+            }
+        }
+    }
+
+    private static void outputImage(File outFile, String outputPath) throws IOException {
         //压缩图片 第一个参数是原图路径 后面那个路径是压缩以后的输出路径
-        Thumbnails.of("D:\\MyConfiguration\\pengdong.guo\\Desktop\\Screenshot_2019-05-19-15-30-42-275_com.miui.video.png")
-                .outputQuality(1f)
-                .scale(1f)
-                .toFile("D:\\MyConfiguration\\pengdong.guo\\Desktop\\1.jpg");
+        Thumbnails.of(outFile).outputQuality(0.9f).scale(1f).toFile(outputPath);
     }
 }
